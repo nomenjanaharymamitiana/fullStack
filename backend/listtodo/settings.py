b9d2509e8e6b63bcef2114468048c7fb504f909b
Mamitiana Daniel <tinamamy28@gmail.com>
2026-03-23 12:39:04 +0300

    Fix: Ajout de la configuration TEMPLATES pour Django Admin

--- backend/listtodo/settings.py
+++ backend/listtodo/settings.py
@@ -73,3 +73,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
+
